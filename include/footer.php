8702bbf338c9b1b833ce4c44004fbab4cc161dde
曽我 <n.soga@sunplacom>
2020-01-30 15:04:33 +0900

    vol.1

---
+++ include/footer.php
@@ -0,0 +1,29 @@
+<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>
+<script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/jquery-migrate/1.4.1/jquery-migrate.min.js' defer charset='UTF-8'></script>
+<script async src="//cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/lodash@4.17.5/lodash.min.js"></script>
+<script src="js/lazyload.min.js"></script>
+<script async src="js/inview.min.js"></script>
+<script async type="text/javascript" src='js/config.js'></script>
+<script async type="text/javascript" src='js/inview_setting.js'></script>
+<script type="text/javascript" src='js/bxslider_setting.js'></script>
+<script async src="js/lightbox.min.js"></script>
+<script>
+    window.WebFontConfig = {
+      google: { families: ['Noto+Sans+JP:100,300,400,500,700,900','Noto Serif JP:100,300,400,500,700,900','Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i'] },
+      active: function() {
+        sessionStorage.fonts = true;
+      }
+    };
+
+    (function() {
+      var wf = document.createElement('script');
+      wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+      wf.type = 'text/javascript';
+      wf.async = 'true';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(wf, s);
+    })();
+</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'include/header.php'; ?>
+<!--コンテンツ-->
+<main class="lp_main wbf_lp_191218_WBF_lp_189_back">
+    <?php include 'include/index/index_01.php'; ?>
+</main>
+<!--コンテンツ-->
+<?php include 'include/footer.php'; ?>
